7b21a98f7f2e521d3e2c6621bcd1bb98d99306e6
ZemtsovSD <ZemtsovSD@310D-10ssd>
2022-09-17 14:04:06 +0700

    Добавьте файлы проекта.

---
+++ BaseLab3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BaseLab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите число");
+                int N = int.Parse(Console.ReadLine());
+                bool flag = true;
+                for (int i = 2; i < N - 1; i++)
+                {
+                    if (N % i == 0) flag = false;
+
+                }
+                if (flag)
+                {
+                    Console.WriteLine("Prime Number:");
+
+                }
+                else
+                {
+                    Console.WriteLine("Composite Number:");
+                }
+            }
+           
+        }
+    }
+}
---
+++ BaseLab4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BaseLab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Введите число");
+            int N = int.Parse(Console.ReadLine());
+            bool[] A = new bool[N];
+            double sqrt = Math.Sqrt(N) + 1;
+            for (int i = 2; i < N - 1; i++)
+            {
+                if (!A[i])
+                {
+                    for (int j = i * i; j < N; j +=i)
+                    {
+                        A[j] = true; 
+
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            for (int i =2; i < N; i++)
+            {
+                if (!A[i])
+                    Console.Write("{0} ", i);
+            }
+            
+        }
+    }
+}
